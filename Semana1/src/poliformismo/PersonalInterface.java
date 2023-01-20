2e88b6fba5322437cdf29be3472311a8ae408ebe
OLIV97 <olivera.scylla97@gmail.com>
2023-01-19 19:50:34 -0600

    Documentation Poliformismo

--- Semana1/src/poliformismo/PersonalInterface.java
+++ Semana1/src/poliformismo/PersonalInterface.java
@@ -4,16 +4,22 @@ import java.util.Date;
 
 public interface PersonalInterface {
 
+	//metodo para saber en que area trabaja
 	String workinIn();
 	
+	//metodo para saber en
 	String arriving();
 
+	//set hora entrada
 	void setEntrada(Date time);
-
+	
+	//get hora entrada
 	Date getEntrada();
 	
+	//set hora salida
 	void setSalida(Date time);
 
+	//get hora salida
 	Date getSalida();
 	
 }
--- Semana1/src/poliformismo/Principal.java
+++ Semana1/src/poliformismo/Principal.java
@@ -8,8 +8,10 @@ public class Principal extends Thread {
 
 	public static void main(String[] args) {
 		
+		//lista del Personal
 		List<PersonalInterface> personal = new ArrayList();
 		
+	
 		personal.add(new Administrativo("Enrique segoviano", "Director", "Auto"));
 		personal.add(new Docente("sotanita del socorro", "Taller de base de datos", "Autobus"));
 		personal.add(new Intendencia("Susanito perez", "Mantenimiento", "Taxi"));
@@ -20,18 +22,23 @@ public class Principal extends Thread {
         for(PersonalInterface p1 :personal) {
         	try {
         		
+        		//simular el tiempo en que esta en frente de la checadora
 	        	sleep(2000);
-	        		
+	        	
 	    		Calendar date = Calendar.getInstance();
 	    		
+	    		//El personal captura la entrada del personal
 	    		p1.setEntrada(date.getTime());
+	    		
 	    		System.out.println("Hora de entrada: "+p1.getEntrada());
 	    		System.out.println(p1.workinIn());
 	    		System.out.println(p1.arriving());
 	        	
+	    		//simula el timepo de trabajo para salir 
 	        	sleep(2000);
 	
 	    		date = Calendar.getInstance();
+	    		//El personal captura la salida 
 	    		p1.setSalida(date.getTime());
 	    		System.out.println("Hora de salida: "+p1.getSalida());
 	    		System.out.println("-----------------------------");
